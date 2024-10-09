ea06481311d18b0fcad08adc92666397ce9ed535
marsodeb <marceaud.pro@gmail.com>
2024-10-09 20:12:29 +0200

    adding components Banner + style

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,7 +1,16 @@
 import '../../sass/base.scss'
+import homebg from '../../assets/home_bg.jpg'
+import Banner from '../../components/Banner/banner';
 
 function Home() {
-
+  return (
+    <section className='home'>
+      <Banner
+        cover={homebg}
+        title={"Chez vous, partout et ailleurs"}
+      />
+    </section >
+  )
 }
 
 export default Home;
