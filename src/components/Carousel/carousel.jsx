dbd74bc359f3c46c89f0588220eeee98c48b958a
marsodeb <marceaud.pro@gmail.com>
2024-10-12 16:14:37 +0200

    adding animation on carousel

--- src/components/Carousel/carousel.jsx
+++ src/components/Carousel/carousel.jsx
@@ -1,18 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './carousel.scss';
 import arrow_left from '../../assets/arrow_left.svg';
 import arrow_right from '../../assets/arrow_right.svg';
 
 function Carousel({ img }) {
-
     const [currentIndex, setCurrentIndex] = useState(0);
+    const [isAnimating, setIsAnimating] = useState(false);
 
     const nextImage = () => {
-        setCurrentIndex((prevIndex) => (prevIndex + 1) % img.length);
+        setIsAnimating(true);
+        setTimeout(() => {
+            setCurrentIndex((prevIndex) => (prevIndex + 1) % img.length);
+            setIsAnimating(false);
+        }, 200);
     };
 
     const prevImage = () => {
-        setCurrentIndex((prevIndex) => (prevIndex - 1 + img.length) % img.length);
+        setIsAnimating(true);
+        setTimeout(() => {
+            setCurrentIndex((prevIndex) => (prevIndex - 1 + img.length) % img.length);
+            setIsAnimating(false);
+        }, 200);
     };
 
     if (!img || img.length === 0) {
@@ -21,9 +29,13 @@ function Carousel({ img }) {
 
     return (
         <div className='carousel'>
-            <img className='carousel-arrow-left' src={arrow_left} alt='Précédent' onClick={prevImage}></img>
-            <img className='carousel-arrow-right' src={arrow_right} alt='Suivante' onClick={nextImage}></img>
-            <img className='carousel-img' src={img[currentIndex]} alt={`${currentIndex + 1}`} />
+            <img className='carousel-arrow-left' src={arrow_left} alt='Précédent' onClick={prevImage} />
+            <img className='carousel-arrow-right' src={arrow_right} alt='Suivante' onClick={nextImage} />
+            <img
+                className={`carousel-img ${!isAnimating ? 'active' : ''}`}
+                src={img[currentIndex]}
+                alt={`${currentIndex + 1}`}
+            />
             <span className='carousel-counter'>{currentIndex + 1} / {img.length}</span>
         </div>
     );
