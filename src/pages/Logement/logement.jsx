27982d10a354093e31693d9eeff8d1ff326eded8
marsodeb <marceaud.pro@gmail.com>
2024-10-12 11:42:44 +0200

    adding rating & keyworkd component + style + finish logement page

--- src/pages/Logement/logement.jsx
+++ src/pages/Logement/logement.jsx
@@ -4,6 +4,9 @@ import './logement.scss';
 import useFetch from '../../utils/useFetch';
 import { useParams } from "react-router-dom";
 import Collapse from '../../components/Collapse/collapse';
+import Rating from '../../components/Rating/rating';
+import Keyword from '../../components/Keyword/keyword';
+import React from 'react';
 
 function Logement() {
 
@@ -28,6 +31,36 @@ function Logement() {
         <section className='logement'>
             <Carousel
                 img={response.pictures} />
+            <div className='title-host'>
+                <div className='title'>
+                    <h1 className='logement-title'>{response.title}</h1>
+                    <p className='location'>{response.location}</p>
+                </div>
+                <div className='host'>
+                    <span className='host-name'>
+                        {response.host.name.split(' ').map((name, index) => (
+                            <React.Fragment key={index}>
+                                {name}
+                                {index === 0 && <br />}
+                            </React.Fragment>
+                        ))}
+                    </span>
+                    <img className='host-img' src={response.host.picture} alt={response.host.name}></img>
+                </div>
+            </div>
+            <div className='keyword-rating'>
+                <div className='keyword'>
+                    {response.tags.map((item, index) => (
+                        <Keyword
+                            key={index}
+                            word={response.tags[index]}
+                        />
+                    ))}
+                </div>
+                <Rating
+                    star={response.rating}
+                />
+            </div>
             <div className='logement-collapse'>
                 <Collapse
                     title={"Description"}
